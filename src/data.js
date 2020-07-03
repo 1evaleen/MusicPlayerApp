144bdc530b8d07dca4e18c823e42ca2d3b401dbc
1evaleen <64602269+1evaleen@users.noreply.github.com>
2020-07-03 14:57:31 +0100

    move to src

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const imagesPath = './images';
+const audioPath = './audio';
+
+const musicData = {
+    data: [
+        {
+            id: 'a',
+            title: 'Back To Black',
+            artist: 'Amy Winehouse',
+            song: 'Playlist/Amy-Winehouse-Back-To-Black.mp3',
+            image: `${imagesPath} + /back-to-black.jfif`
+        },
+        {
+            id: 'b',
+            title: 'Bella Ciao',
+            artist: 'Original',
+            song: 'Playlist/Bella-Ciao-ORIGINALE.mp3',
+            image: './Playlist/images/Money-Heist-Mask.jpg'
+        },
+        {
+            id: 'c',
+            title: 'Jolene',
+            artist: 'Miley Cyrus',
+            song: 'Playlist/MileyCyrusJolene.mp3',
+            image: 'Playlist/images/jolene.jfif'
+        },
+        {
+            id: 'd',
+            title: 'Stressed Out',
+            artist: 'Twenty One Pilots',
+            song: 'Playlist/twentyonepilotsStressedOut.mp3',
+            image: 'Playlist/images/stressed-out.png'
+        },
+        {
+            id: 'e',
+            title: 'Shallow',
+            artist: 'A Star Is Born',
+            song: 'Playlist/Shallow-fromAStarIsBorn.mp3',
+            image: 'Playlist/images/shallow.jfif'
+        },
+        {
+            id: 'a',
+            title: 'Back To Black',
+            artist: 'Amy Winehouse',
+            song: 'Playlist/Amy-Winehouse-Back-To-Black.mp3',
+            image: `${imagesPath} + /images/back-to-black.jfif`
+        },
+
+        
+    ]
+};
+export default musicData;
