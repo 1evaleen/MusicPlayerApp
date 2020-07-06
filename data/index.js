d7b3b8d0f88fc64e523bfe43db6b97b52617c49d
1evaleen <64602269+1evaleen@users.noreply.github.com>
2020-07-06 12:14:02 +0100

    adding new data

--- data/index.js
+++ data/index.js
@@ -1,4 +1,5 @@
-// const imagesPath = './images';
+const imagesPath = '../data/images';
+const audioPath = '../data/audio';
 
 
 const musicData = [
@@ -6,38 +7,82 @@ const musicData = [
         id: 'a',
         title: 'Back To Black',
         artist: 'Amy Winehouse',
-        song: '../data/audio/Amy-Winehouse-Back-To-Black.mp3',
-        image: '../data/images/back-to-black.jfif'
+        song: `${audioPath}/Amy-Winehouse-Back-To-Black.mp3`,
+        image: `${imagesPath}/back-to-black.jfif`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
     },
-    // {
-    //     id: 'b',
-    //     title: 'Bella Ciao',
-    //     artist: 'Original',
-    //     song: 'Playlist/Bella-Ciao-ORIGINALE.mp3',
-    //     image: './Playlist/images/Money-Heist-Mask.jpg'
-    // },
-    // {
-    //     id: 'c',
-    //     title: 'Jolene',
-    //     artist: 'Miley Cyrus',
-    //     song: 'Playlist/MileyCyrusJolene.mp3',
-    //     image: 'Playlist/images/jolene.jfif'
-    // },
-    // {
-    //     id: 'd',
-    //     title: 'Stressed Out',
-    //     artist: 'Twenty One Pilots',
-    //     song: 'Playlist/twentyonepilotsStressedOut.mp3',
-    //     image: 'Playlist/images/stressed-out.png'
-    // },
-    // {
-    //     id: 'e',
-    //     title: 'Shallow',
-    //     artist: 'A Star Is Born',
-    //     song: 'Playlist/Shallow-fromAStarIsBorn.mp3',
-    //     image: 'Playlist/images/shallow.jfif'
-    // },
-
+    {
+        id: 'b',
+        title: 'Bella Ciao',
+        artist: 'Original',
+        song: `${audioPath}/Bella-Ciao-ORIGINALE.mp3`,
+        image: `${imagesPath}/Money-Heist-Mask.jpg`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'c',
+        title: 'Hound Dog',
+        artist: 'Elvis Presley',
+        song: `${audioPath}/Elvis-Presley-hound-dog.mp3`,
+        image: `${imagesPath}/hound-dog.jpg`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'd',
+        title: 'Poker Face',
+        artist: 'Lady Gaga',
+        song: `${audioPath}/lady-gaga-poker-face.mp3`,
+        image: `${imagesPath}/poker-face.png`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'e',
+        title: 'Jolene',
+        artist: 'Miley Cyrus',
+        song: `${audioPath}/MileyCyrusJolene.mp3`,
+        image: `${imagesPath}/jolene.jfif`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'f',
+        title: 'Pretty Woman',
+        artist: 'Roy Orbison',
+        song: `${audioPath}/roy-orbison-pretty-woman.mp3`,
+        image: `${imagesPath}/roy_orbison_pretty_woman.jpg`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'g',
+        title: 'Wind of Change',
+        artist: 'Scorpions',
+        song: `${audioPath}/scorpions-wind-of-change.mp3`,
+        image: `${imagesPath}/Scorpions-wind-video.jpg`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'h',
+        title: 'Shallow',
+        artist: 'A Star is Born',
+        song: `${audioPath}/Shallow-fromAStarIsBorn.mp3`,
+        image: `${imagesPath}/shallow.jfif`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'i',
+        title: 'Sound of Silence',
+        artist: 'Simon and Garfunkel',
+        song: `${audioPath}/simon-garfunkel-sound-of-silence.mp3`,
+        image: `${imagesPath}/simon-and-garfunkel.jpg`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
+    {
+        id: 'j',
+        title: 'Stressed Out',
+        artist: 'Twenty One Pilots',
+        song: `${audioPath}/twentyonepilotsStressedOut.mp3`,
+        image: `${imagesPath}/stressed-out.png`,
+        details: 'details about the song to be added here - perhaps a summary or details about the artist and version'
+    },
     
 ]
 
